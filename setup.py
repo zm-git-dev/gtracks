3d3c9c63010d3ddb82c3fb9fad1c0698a2c7ca90
Anthony Aylward <aaylward@eng.ucsd.edu>
2020-02-24 16:33:51 -0800

    advance version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='gtracks',
-    version='1.0.8',
+    version='1.0.9',
     author='Anthony Aylward',
     author_email='aaylward@eng.ucsd.edu',
     description='Plot genome track data',
